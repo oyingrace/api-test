5d8f728b3329d06335de8294692ff3b816b0d329
oyingrace <oyingrace307@gmail.com>
2024-10-17 19:50:10 +0100

    add

--- test.ts
+++ test.ts
@@ -1,4 +1,10 @@
 export async function GET() {
+  try {
     return new Response('Hello, world!');
+  } catch (error) {
+    console.error('Error in GET function:', error);
+    return new Response('Internal Server Error', { status: 500 });
   }
+}
+
   
